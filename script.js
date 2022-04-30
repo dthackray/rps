d0927ceb88622406433aa113c427e6d8080eb455
Dan Thackray <djthackray@gmail.com>
2022-04-30 20:23:26 +0100

    Create initial computerPlay function

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world!")
+function computerPlay() {
+    const choiceNum = Math.floor(Math.random() * 3) + 1
+    if (choiceNum === 1) {
+        console.log("Rock")
+    } else if (choiceNum === 2) {
+        console.log("Paper")
+    } else {
+        console.log("Scissors")
+    }
+}
+
+computerPlay()
