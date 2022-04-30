b8e2c6da1268fd97966f91593493495d7162f9c3
Dan Thackray <djthackray@gmail.com>
2022-04-30 20:40:28 +0100

    Create playRound function

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 function computerPlay() {
     const choiceNum = Math.floor(Math.random() * 3) + 1
     if (choiceNum === 1) {
-        console.log("Rock")
+        return "Rock"
     } else if (choiceNum === 2) {
-        console.log("Paper")
+        return "Paper"
     } else {
-        console.log("Scissors")
+        return "Scissors"
     }
 }
 
-computerPlay()
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return "Draw! Play again"
+    } else if (playerSelection.toLowerCase() === "rock") {
+        if (computerSelection.toLowerCase() === "scissors") {
+            return "You win! Rock beats Scissors"
+        } else {
+            return "You lose! Paper beats Rock"
+        }
+    } else if (playerSelection.toLowerCase() === "paper") {
+        if (computerSelection.toLowerCase() === "rock") {
+            return "You win! Paper beats Rock"
+        } else {
+            return "You lose! Scissors beats Paper"
+        }
+    } else {
+        if (computerSelection.toLowerCase() === "paper") {
+            return "You win! Scissors beats Paper"
+        } else {
+            return "You lose! Rock beats Scissors"
+        }
+    }
+}
+
+const playerSelection = "Paper"
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
